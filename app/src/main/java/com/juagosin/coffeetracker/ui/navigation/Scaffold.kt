98d5a2bc99e1e5cca72cd82d7f157ad97beb83ca
juagosin <juagosin@gmail.com>
2025-10-28 08:53:45 +0100

    feat: Add Floating Action Button to add coffees
    Adds a Floating Action Button (FAB) to the main screen layout. This button will eventually be used to add a new coffee entry.
    
    - Implements the FAB within the `CoffeeScaffold`.
    - Adds a new `coffeecup.png` drawable to be used as the FAB icon.
    - Includes a content description string `fab_description` for accessibility.

--- app/src/main/java/com/juagosin/coffeetracker/ui/navigation/Scaffold.kt
+++ app/src/main/java/com/juagosin/coffeetracker/ui/navigation/Scaffold.kt
@@ -1,11 +1,19 @@
 package com.juagosin.coffeetracker.ui.navigation
 
+import android.R.attr.resource
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.juagosin.coffeetracker.Greeting
+import com.juagosin.coffeetracker.R
 
 
 @Composable
@@ -18,6 +26,18 @@ fun CoffeeScaffold( modifier: Modifier
         },
         bottomBar = {
             CoffeBottomBar()
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /* TODO FAB logic */ }
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.coffeecup),
+                    contentDescription = stringResource(R.string.fab_description),
+                    modifier = Modifier.size(28.dp)
+
+                )
+            }
         }
     ) { innerPadding ->
         Greeting(
