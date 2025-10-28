fa55eda2f5475b359a1a6dc63bc78743c84a8e14
juagosin <juagosin@gmail.com>
2025-10-28 08:30:40 +0100

    feat: Implement basic navigation structure
    Adds the basic UI structure for the app using Material 3 Scaffold.
    
    - Creates `CoffeeScaffold` to hold the main app layout.
    - Extracts `CoffeTopAppBar` and `CoffeBottomBar` into their own composable files.
    - Refactors `MainActivity` to use the new `CoffeeScaffold`.
    - Adds a string resource for the app title.

--- app/src/main/java/com/juagosin/coffeetracker/ui/navigation/TopAppBar.kt
+++ app/src/main/java/com/juagosin/coffeetracker/ui/navigation/TopAppBar.kt
@@ -3,14 +3,12 @@ package com.juagosin.coffeetracker.ui.navigation
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SearchBarDefaults.colors
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.juagosin.coffeetracker.R
 
