25a78d24a4f6d2dccd66b992186b30954f5261ed
juagosin <juagosin@gmail.com>
2025-10-28 09:05:32 +0100

    refactor: Integrate Hilt, Room, and Coroutines
    This commit adds and configures essential libraries for dependency injection, database persistence, and asynchronous programming.
    
    - Integrates Hilt for dependency injection by adding the `hilt-android` plugin and necessary dependencies.
    - Adds Room for local database storage, including runtime, KTX, and compiler dependencies.
    - Incorporates Kotlin Coroutines for managing background threads.
    - Adds the KSP plugin to support annotation processing for Hilt and Room.
    - Upgrades the Kotlin version to `2.2.20`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -58,4 +60,14 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    //DI
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.navigation)
+    ksp(libs.hilt.compiler)
+    //ROOM
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.android)
 }
