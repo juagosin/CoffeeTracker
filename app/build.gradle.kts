72a62ff101b6357aa8f01542cbc7d631193f6521
juagosin <juagosin@gmail.com>
2025-11-14 07:07:06 +0100

    build: Bump version to 3.1.6
    Increases the application's version code to 2 and version name to "3.1.6".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.calleserpis.coffeetracker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "3.1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
